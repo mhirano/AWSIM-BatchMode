9b457a1e4580ea6d06186fa716bc30afcab01787
masahiro.dll <masahiro.dll@gmail.com>
2024-04-04 21:42:36 +0900

    add main.py to build and run simulation while changing camera parameters

---
+++ bayes_util.py
@@ -0,0 +1,177 @@
+from pathlib import Path
+
+def GenerateScriptToChangeParameter(projectRootPath, **kwargs):
+
+    for k, v in kwargs.items():
+        if k == 'camera1_pos_x':
+            camera1_pos_x = v
+        if k == 'camera1_pos_y':
+            camera1_pos_y = v
+        if k == 'camera1_pos_z':
+            camera1_pos_z = v
+        if k == 'camera1_angle_x':
+            camera1_angle_x = v
+        if k == 'camera1_angle_y':
+            camera1_angle_y = v
+        if k == 'camera1_angle_z':
+            camera1_angle_z = v
+        if k == 'camera1_focalLength':
+            camera1_focalLength = v
+
+        if k == 'camera2_pos_x':
+            camera2_pos_x = v
+        if k == 'camera2_pos_y':
+            camera2_pos_y = v
+        if k == 'camera2_pos_z':
+            camera2_pos_z = v
+        if k == 'camera2_angle_x':
+            camera2_angle_x = v
+        if k == 'camera2_angle_y':
+            camera2_angle_y = v
+        if k == 'camera2_angle_z':
+            camera2_angle_z = v
+        if k == 'camera2_focalLength':
+            camera2_focalLength = v
+
+        if k == 'camera3_pos_x':
+            camera3_pos_x = v
+        if k == 'camera3_pos_y':
+            camera3_pos_y = v
+        if k == 'camera3_pos_z':
+            camera3_pos_z = v
+        if k == 'camera3_angle_x':
+            camera3_angle_x = v
+        if k == 'camera3_angle_y':
+            camera3_angle_y = v
+        if k == 'camera3_angle_z':
+            camera3_angle_z = v
+        if k == 'camera3_focalLength':
+            camera3_focalLength = v
+
+    template_path=Path('ChangeCameraParameters_template.cs')
+    script_path=projectRootPath / Path('Assets/AWSIM/Scripts/ChangeCameraParameters.cs')
+
+    fr = open(str(template_path),'r')
+    fw = open(str(script_path), 'w')
+    while True:
+        line = fr.readline()
+
+        # Break if EOF
+        if line == '':
+            break
+
+        # Camera1 parameters
+        if line.find("CAMERA1_POSITION_X") != -1:
+            line = line.replace("old_pos.x", str(camera1_pos_x))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA1_POSITION_Y") != -1:
+            line = line.replace("old_pos.y", str(camera1_pos_y))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA1_POSITION_Z") != -1:
+            line = line.replace("old_pos.z", str(camera1_pos_z))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA1_ANGLE_X") != -1:
+            line = line.replace("old_angle.x", str(camera1_angle_x))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA1_ANGLE_Y") != -1:
+            line = line.replace("old_angle.y", str(camera1_angle_y))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA1_ANGLE_Z") != -1:
+            line = line.replace("old_angle.z", str(camera1_angle_z))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA1_FOCALLENGTH") != -1:
+            line = line.replace("old_focalLength", str(camera1_focalLength))
+            print(line, file=fw, end='')
+            continue
+
+
+        # Camera2 parameters
+        if line.find("CAMERA2_POSITION_X") != -1:
+            line = line.replace("old_pos.x", str(camera2_pos_x))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA2_POSITION_Y") != -1:
+            line = line.replace("old_pos.y", str(camera2_pos_y))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA2_POSITION_Z") != -1:
+            line = line.replace("old_pos.z", str(camera2_pos_z))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA2_ANGLE_X") != -1:
+            line = line.replace("old_angle.x", str(camera2_angle_x))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA2_ANGLE_Y") != -1:
+            line = line.replace("old_angle.y", str(camera2_angle_y))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA2_ANGLE_Z") != -1:
+            line = line.replace("old_angle.z", str(camera2_angle_z))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA2_FOCALLENGTH") != -1:
+            line = line.replace("old_focalLength", str(camera2_focalLength))
+            print(line, file=fw, end='')
+            continue
+
+
+        # Camera3 parameters
+        if line.find("CAMERA3_POSITION_X") != -1:
+            line = line.replace("old_pos.x", str(camera3_pos_x))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA3_POSITION_Y") != -1:
+            line = line.replace("old_pos.y", str(camera3_pos_y))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA3_POSITION_Z") != -1:
+            line = line.replace("old_pos.z", str(camera3_pos_z))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA3_ANGLE_X") != -1:
+            line = line.replace("old_angle.x", str(camera3_angle_x))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA3_ANGLE_Y") != -1:
+            line = line.replace("old_angle.y", str(camera3_angle_y))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA3_ANGLE_Z") != -1:
+            line = line.replace("old_angle.z", str(camera3_angle_z))
+            print(line, file=fw, end='')
+            continue
+
+        if line.find("CAMERA3_FOCALLENGTH") != -1:
+            line = line.replace("old_focalLength", str(camera3_focalLength))
+            print(line, file=fw, end='')
+            continue
+
+        print(line, file=fw, end='')
+
+
+    fr.close()
+    fw.close()
--- main.py
+++ main.py
@@ -1,155 +1,58 @@
-# 環境設定
-# 自分の環境に合わせて変更してください
-template_path="ChangeCameraParameters.cs"
-script_path="ChangeCameraParameters_fill.cs"
-# script_path="/home/mhirano/Documents/Projects/AWSIM/Assets/AWSIM/Scripts/ChangeCameraParameters.cs"
+import subprocess
+import bayes_util
+
+#### 自分の環境に合わせて変更 ↓ここから
+awsimProjectRootPath = "/home/mhirano/Documents/Projects/AWSIM" # AWSIMのディレクトリ
+unityBinaryPath = "/home/mhirano/Unity/Hub/Editor/2021.1.7f1/Editor/Unity"
+#### 自分の環境に合わせて変更 ↑ここまで
+
+for i in range(5):
+    height = i
+
+    # Create a unity script that changes camera parameters (position/angle/focalLength)
+    bayes_util.GenerateScriptToChangeParameter(
+        awsimProjectRootPath,
+        camera1_pos_x=225,
+        camera1_pos_y=height,
+        camera1_pos_z=-255,
+        camera1_angle_x=0,
+        camera1_angle_y=-60,
+        camera1_angle_z=0,
+        camera1_focalLength=25,
+        camera2_pos_x=170,
+        camera2_pos_y=height,
+        camera2_pos_z=-235,
+        camera2_angle_x=0,
+        camera2_angle_y=-240,
+        camera2_angle_z=0,
+        camera2_focalLength=25,
+        camera3_pos_x=200,
+        camera3_pos_y=height,
+        camera3_pos_z=-260,
+        camera3_angle_x=30,
+        camera3_angle_y=5,
+        camera3_angle_z=0,
+        camera3_focalLength=25
+        )
+
+    # Build a unity player with the specified camera paraemters
+    commandToBuildPlayer = [ 
+        unityBinaryPath,
+        "-quit",
+        "-batchmode",
+        "-buildTarget", 
+        "Linux64",
+        "-executeMethod",
+        "BuildPlayer.MyBuild",
+        "-projectPath",
+        "/home/mhirano/Documents/Projects/AWSIM"
+        ]
+    subprocess.call(commandToBuildPlayer)
+
+    # Run the built player
+    commandToRunPlayer =[
+        awsimProjectRootPath+"/Linux/Player"
+    ]
+    subprocess.call(commandToRunPlayer)
 
 
-# 新しいパラメータたち
-camera1_pos_x = 225.2
-camera1_pos_y = 2
-camera1_pos_z = -255
-camera1_angle_x = 0
-camera1_angle_y = -60
-camera1_angle_z = 0
-camera1_focalLength = 25
-
-camera2_pos_x = 170
-camera2_pos_y = 2
-camera2_pos_z = -235
-camera2_angle_x = 0
-camera2_angle_y = -240
-camera2_angle_z = 0
-camera2_focalLength = 25
-
-camera3_pos_x = 200
-camera3_pos_y = 2
-camera3_pos_z = -260
-camera3_angle_x = 0
-camera3_angle_y = 0
-camera3_angle_z = 0
-camera3_focalLength = 25
-
-fr = open(template_path, 'r')
-fw = open(script_path, 'w')
-while True:
-    line = fr.readline()
-
-    # Break if EOF
-    if line == '':
-        break
-
-    if line.find("CAMERA1_POSITION_X") != -1:
-        line = line.replace("old_pos.x", str(camera1_pos_x))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA1_POSITION_Y") != -1:
-        line = line.replace("old_pos.y", str(camera1_pos_y))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA1_POSITION_Z") != -1:
-        line = line.replace("old_pos.z", str(camera1_pos_z))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA1_ANGLE_X") != -1:
-        line = line.replace("old_angle.x", str(camera1_angle_x))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA1_ANGLE_Y") != -1:
-        line = line.replace("old_angle.y", str(camera1_angle_y))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA1_ANGLE_Z") != -1:
-        line = line.replace("old_angle.z", str(camera1_angle_z))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA1_FOCALLENGTH") != -1:
-        line = line.replace("old_focalLength", str(camera1_focalLength))
-        print(line, file=fw, end='')
-        continue
-
-
-
-    if line.find("CAMERA2_POSITION_X") != -1:
-        line = line.replace("old_pos.x", str(camera2_pos_x))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA2_POSITION_Y") != -1:
-        line = line.replace("old_pos.y", str(camera2_pos_y))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA2_POSITION_Z") != -1:
-        line = line.replace("old_pos.z", str(camera2_pos_z))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA2_ANGLE_X") != -1:
-        line = line.replace("old_angle.x", str(camera2_angle_x))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA2_ANGLE_Y") != -1:
-        line = line.replace("old_angle.y", str(camera2_angle_y))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA2_ANGLE_Z") != -1:
-        line = line.replace("old_angle.z", str(camera2_angle_z))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA2_FOCALLENGTH") != -1:
-        line = line.replace("old_focalLength", str(camera2_focalLength))
-        print(line, file=fw, end='')
-        continue
-
-
-
-    if line.find("CAMERA3_POSITION_X") != -1:
-        line = line.replace("old_pos.x", str(camera3_pos_x))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA3_POSITION_Y") != -1:
-        line = line.replace("old_pos.y", str(camera3_pos_y))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA3_POSITION_Z") != -1:
-        line = line.replace("old_pos.z", str(camera3_pos_z))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA3_ANGLE_X") != -1:
-        line = line.replace("old_angle.x", str(camera3_angle_x))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA3_ANGLE_Y") != -1:
-        line = line.replace("old_angle.y", str(camera3_angle_y))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA3_ANGLE_Z") != -1:
-        line = line.replace("old_angle.z", str(camera3_angle_z))
-        print(line, file=fw, end='')
-        continue
-
-    if line.find("CAMERA3_FOCALLENGTH") != -1:
-        line = line.replace("old_focalLength", str(camera3_focalLength))
-        print(line, file=fw, end='')
-        continue
-
-    print(line, file=fw, end='')
-
-
-fr.close()
-fw.close()
